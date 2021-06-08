9b60bcc3507ac11be1c54470305b4c6818ea9236
mario <mario0000o7@wp.pl>
2021-06-08 19:22:53 +0200

    koncowa v3

--- wroclaw/sidebar_for_pages.js
+++ wroclaw/sidebar_for_pages.js
@@ -1,19 +1,36 @@
+var nav = false;
+
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("myFooter").style.marginLeft = "250px";
+  nav = true;
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("myFooter").style.marginLeft = "0";
+  nav = false;
+}
+
+function toggleNav() {
+  if(nav == false){
+    openNav()
   }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+  else{
+    closeNav()
   }
 
-  function DarkMode(){
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-    /*document.getElementById("mySidenav").toggle("dark-mode");*/
-    
+}
 
-    var x =document.getElementsByClassName("nav");
-    x.classList.toggle("dark-mode");
-  }
+function DarkMode(){
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+  /*document.getElementById("mySidenav").toggle("dark-mode");*/
+
+
+  var x =document.getElementsByClassName("nav");
+  x.classList.toggle("dark-mode");
+}
+
